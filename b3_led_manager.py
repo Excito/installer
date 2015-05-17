97daa77a81d2761589a289ba2208e65da307741b
Charles Leclerc <leclerc.charles@gmail.com>
2015-05-17 00:22:01 -0400

    Remove blink management on the b3. Added network configuration and ini file.

---
+++ b3_led_manager.py
@@ -0,0 +1,58 @@
+from time import sleep
+
+def b3_set_color(color):
+    r = 0
+    g = 0
+    b = 0
+    if color == 'red':
+        r = 1
+    elif color == 'green':
+        g = 1
+    elif color == 'blue':
+        b = 1
+    elif color == 'cyan':
+        b, g = 1, 1
+    elif color == 'yellow':
+        r, g = 1, 1
+    elif color == 'purple':
+        r, b = 1, 1
+    elif color == 'white':
+        r, g, b = 1, 1, 1
+
+    r_f = open('/sys/class/leds/bubba3:red:error/brightness', 'w')
+    r_f.write(str(r))
+    r_f.close()
+
+    g_f = open('/sys/class/leds/bubba3:green:programming/brightness', 'w')
+    g_f.write(str(g))
+    g_f.close()
+
+    b_f = open('/sys/class/leds/bubba3:blue:active/brightness', 'w')
+    b_f.write(str(b))
+    b_f.close()
+
+def led_install():
+    b3_set_color('cyan')
+
+def led_error():
+    b3_set_color('red')
+
+def led_rescue():
+    b3_set_color('green')
+
+def b3_print_integer(n):
+    for i in n:
+        i = int(i)
+        if i == 0:
+            b3_set_color('purple')
+            sleep(0.2)
+            b3_set_color('black')
+            sleep(0.2)
+
+        while i > 0:
+            b3_set_color('yellow')
+            sleep(0.2)
+            b3_set_color('black')
+            sleep(0.2)
+            i -= 1
+        sleep(0.3)
--- installer.py
+++ installer.py
@@ -1,15 +1,20 @@
+#! /usr/bin/python
+
 __author__ = 'Charles Leclerc'
 
 import daemon
-from led_manager import *
 import logging
 import os
 import os.path
-from time import sleep
 from subprocess import Popen, PIPE
 from threading import Thread
 import sys
+import ConfigParser
+import netifaces
+from signal import signal, SIGTERM
 
+CONFIG_FILE = '/mnt/usb/install/install.ini'
+PID_FILE = '/var/run/installer.pid'
 
 # ------------------------------------
 # ----- Useful functions -------------
@@ -37,6 +42,7 @@ def runcmd1(cmd):
         logging.info("[%s] returned status %i" % (','.join(cmd), p.returncode))
     return p.returncode
 
+
 # Run the specified command redirecting only stderr to logging
 # Returns the process reference and the stderr writing thread
 def runcmd2(cmd):
@@ -51,96 +57,243 @@ def runcmd2(cmd):
     t.start()
     return p, t
 
+
+# write /etc/network/interfaces from configuration
+def configure_network():
+    global config
+    logging.info('Writing /etc/network/interfaces')
+    o = open('/etc/network/interfaces', 'a')
+    for s, i in (('wan', 'eth0'), ('lan', 'eth1')):
+        p = config.get(s, 'proto').strip()
+        if p == 'static' and (not config.has_option(s, 'ipaddr') or not config.has_option(s, 'netmask')):
+            logging.warning('Missing ipaddr or netmask for static configuration of %s ; falling back to DHCP' % (s, ))
+            p = 'dhcp'
+        o.write('\niface %s proto %s' % (i, p))
+        if p == 'static':
+            o.write('\n  address %s\n  netmask  %s' % (config.get(s, 'ipaddr').strip(),
+                                                       config.get(s, 'netmask').strip()))
+            if config.has_option(s, 'gateway'):
+                o.write('\n  gateway %s' % (config.get(s, 'gateway').strip()))
+        o.write('\n')
+    o.close()
+
+
+# Return the relevant numbers from the first address ip found on eth0/eth1
+# class C : last number; class B : last two numbers, etc.
+def get_first_ip_relevant_numbers():
+    for i in 'eth0', 'eth1':
+        addrs = netifaces.ifaddresses(i)
+        if netifaces.AF_INET not in addrs:
+            continue
+        if len(addrs[netifaces.AF_INET]) >= 1 and 'addr' in addrs[netifaces.AF_INET][0]:
+            ip = addrs[netifaces.AF_INET][0]['addr'].split('.')
+            nm = addrs[netifaces.AF_INET][0]['netmask'].split('.')
+            res = []
+            for r, n in map(None, ip, nm):
+                if n == '0':
+                    res.append(r)
+            return res
+    return None
+
+
+# Detect if we're running on a Bubba|2 (if not we're on a B3)
+def is_b2():
+    return os.path.exists('/sys/devices/platform/bubbatwo')
+
+
+def write_pid():
+    o = open(PID_FILE, 'w')
+    o.write('%i\n' % (os.getpid(),))
+    o.close()
+
+
+def loop_ip_forever():
+    if is_b2():
+        if error:
+            led_error()
+        else:
+            led_rescue()
+        return  # not supported on the b2
+    ip = None
+    while True:
+        if error:
+            led_error()
+        else:
+            led_rescue()
+        sleep(2)
+        if not ip:
+            ip = get_first_ip_relevant_numbers()
+        if ip:
+            first = True
+            for n in ip:
+                if first:
+                    first = False
+                else:
+                    b3_set_color('cyan')
+                    sleep(1)
+                b3_set_color('black')
+                sleep(0.5)
+                b3_print_integer(n)
+            sleep(0.2)
+
+
+def daemon_term_handler(signum, frame):
+    os.unlink(PID_FILE)
+    if error:
+        led_error()
+    else:
+        led_rescue()
+    sys.exit(0)
+
 # ------------------------------------
 # ----- Preliminary script -----------
 # ------------------------------------
 
-# Exit the script in early phase due to install key not found/not mounted
-# spawn ifplugd with dhcp on both interfaces and exit
-def early_exit():
-    logging.info("Spawning ifplugd with dhcp on both interfaces")
-    logging.info("Updating /etc/network/interfaces")
-#    net_conf = open('/etc/network/interfaces', 'a')
-#    net_conf.write("\niface eth0 inet dhcp\niface eth1 inet dhcp\n")
-#    net_conf.close()
-#    runcmd(['/etc/init.d/ifplugd', 'start'])
-    logging.warning("Install script ended with errors")
-    lm1.stop(LED_RED)
-    lm1.join(1)
-    logging.shutdown()
+if os.path.exists(PID_FILE):
+    sys.stderr.write('%s exists ; refusing to run !\n' % (PID_FILE, ))
     sys.exit(1)
 
-# Early log facility : directly on stdout (meaning the console)
-logging.basicConfig(format='%(asctime)s.%(msecs)d - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %I:%M:%S',
-                    level=logging.INFO)
-
-# load and run early led control thread (which will be stopped before forking and restarted after)
-lm1 = LedManager()
-lm1.start()
-# Fast blink => searching/mounting USB key
-lm1.set_led(LED_GREEN, 0.1)
-
-# search and mount USB key
-usb_dev = ''
-attempts = 5
-while attempts:
-    logging.info("Searching for USB install key")
-    for d in [f for f in os.listdir('/sys/block') if f.startswith('sd')]:
-        if '/usb' in os.readlink(os.path.join('/sys/block', d)):
-            usb_dev = d
-            break
-    if usb_dev:
-        break
+# Exit the script in early phase due to install key not found/not mounted or no configuration file found
+def early_exit():
+    global error
+    logging.info("Configuring network with default settings")
+    configure_network()
+    logging.warning("Install script ended with errors")
+    logging.shutdown()
+    if foreground:
+        led_error()
+        sys.exit(1)
     else:
-        sleep(2)
-    attempts -= 1
+        with daemon.DaemonContext():
+            error = True
+            write_pid()
+            signal(SIGTERM, daemon_term_handler)
+            loop_ip_forever()
+            os.unlink(PID_FILE)
+            sys.exit(1)
 
-if attempts:
-    logging.info("Mounting USB install key")
-    if runcmd1(['mount', '/dev/%s1' % (usb_dev, ), '/mnt/usb']):
-        logging.error('Unable to mount USB key !')
+if is_b2():
+    from b2_led_manager import *
+else:
+    from b3_led_manager import *
+
+led_install()
+
+# Foreground run ?
+foreground = len(sys.argv) > 1 and sys.argv[1] == '-f'
+
+if foreground:
+    # Everyting to the console
+    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%I:%M:%S',
+                        level=logging.INFO)
+else:
+    # Early log facility in /root folder
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %I:%M:%S',
+                        level=logging.INFO, filename='/root/install-early.log')
+
+# Configuration defaults
+config = ConfigParser.SafeConfigParser()
+for s in ('wan', 'lan'):
+    config.add_section(s)
+    config.set(s, 'proto', 'dhcp')
+config.add_section('general')
+config.set('general', 'reboot', 'true')
+
+# check if USB key is already mounted, search and mount otherwise
+p, t = runcmd2(['cat', '/proc/mounts'])
+usb_mounted = False
+for iline in p.stdout:
+    cc = iline.split()
+    if cc[1] == '/mnt/usb':
+        usb_mounted = True
+t.join()
+p.wait()
+if usb_mounted:
+    logging.info('/mnt/usb already mounted ; skipping USB key detection')
+else:
+    # search and mount USB key
+    usb_dev = ''
+    attempts = 5
+    while attempts:
+        logging.info("Searching for USB install key")
+        for d in [f for f in os.listdir('/sys/block') if f.startswith('sd')]:
+            if '/usb' in os.readlink(os.path.join('/sys/block', d)):
+                usb_dev = d
+                break
+        if usb_dev:
+            break
+        else:
+            sleep(2)
+        attempts -= 1
+
+    if attempts:
+        logging.info("Mounting USB install key")
+        if runcmd1(['mount', '/dev/%s1' % (usb_dev, ), '/mnt/usb']):
+            logging.error('Unable to mount USB key !')
+            early_exit()
+    else:
+        logging.error('Unable to detect USB key !')
+        early_exit()
+
+if os.path.exists(CONFIG_FILE):
+    logging.info('Loading configuration from ' + CONFIG_FILE)
+    if CONFIG_FILE not in config.read(CONFIG_FILE):
+        logging.error('Unable to read configuration file !')
         early_exit()
 else:
-    logging.error('Unable to detect USB key !')
+    logging.error('No configuration file found on USB key !')
     early_exit()
 
-logging.info("Initialization done. Preparing daemon forking")
+configure_network()
 
-# Stopping led manager thread (can't support the fork)
-lm1.stop()
-lm1.join()
+if not foreground:
+    logging.info("Initialization done. Preparing daemon forking")
+    # Logging facility shutdown
+    logging.shutdown()
+    logging.root.handlers = []
 
-# Logging facility shutdown
-logging.shutdown()
-logging.root.handlers = []
 
 # ------------------------------------
 # ----- Main install script ----------
 # ------------------------------------
 
 def do_install():
-    lm2.set_led(LED_CYAN)
-    env_out = open("/mnt/usb/install/env.txt", 'w')
-    p, t = runcmd2(['fw_printenv', 'ethaddr', 'eth1addr', 'key'])
-    for iline in p.stdout:
-        env_out.write(iline)
-    t.join()
-    p.wait()
-    os.system("/sbin/reboot")
+    if not config.has_option('general', 'image'):
+        logging.info('No image in configuration. Exiting leaving the rescue system')
+        return
 
-with daemon.DaemonContext():
-    logging.basicConfig(format='%(asctime)s.%(msecs)d - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %I:%M:%S',
-                    level=logging.INFO, filename="/mnt/usb/install/install.log")
-    logging.info("Running installer daemon process")
-    logging.info("Restarting led manager ...")
-    lm2 = LedManager()
-    lm2.start()
+if foreground:
     try:
         do_install()
-        lm2.stop(LED_GREEN)
+        # we never reboot neither loop with ip in foreground
+        led_rescue()
     except:
-        logging.exception("Exception in the main daemon function :")
-        lm2.stop(LED_RED)
+        logging.exception("Exception in the main function :")
+        led_error()
+        sys.exit(1)
     finally:
         logging.shutdown()
+else:
+    with daemon.DaemonContext():
+        logging.basicConfig(format='%(asctime)s.%(msecs)d - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %I:%M:%S',
+                            level=logging.INFO, filename="/mnt/usb/install/install.log")
+        error = False
 
+        write_pid()
+        signal(SIGTERM, daemon_term_handler)
+
+        logging.info("Running installer daemon process with pid %i" % (os.getpid(), ))
+
+        try:
+            do_install()
+            if config.getboolean('general', 'reboot'):
+                os.system('/sbin/reboot')
+        except:
+            logging.exception("Exception in the main daemon function :")
+            error = True
+        finally:
+            os.unlink(PID_FILE)
+            logging.shutdown()
+
+        loop_ip_forever()
+        os.unlink(PID_FILE)
